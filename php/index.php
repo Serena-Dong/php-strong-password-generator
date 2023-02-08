5b3e9e2620a1e55556b0cf939e83baf9647f04a2
Serena-Dong <serenadong3159@icloud.com>
2023-02-08 17:37:59 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+    </div>
+</body>
+</html>
