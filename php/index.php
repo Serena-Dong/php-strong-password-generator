a68757361cf6f87b970c61c18c43ba4af116e957
Serena-Dong <serenadong3159@icloud.com>
2023-02-08 18:39:05 +0100

    form

--- php/index.php
+++ php/index.php
@@ -9,13 +9,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
 
+    <!-- BS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
     <!-- CSS -->
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <div class="header">
-        <h1>Strong Password Generator</h1>
+    <header>
+       <div class="titles mb-4">
+       <h1>Strong Password Generator</h1>
         <h2>Genera una password sicura</h2>
-    </div>
+       </div>
+    </header>
+    <main>
+        <div class="container d-flex justify-content-center">
+
+            <div class="form-container">
+                <form action="" method="GET">
+                    <!-- Input -->
+                    <div class="password-form mb-3 d-flex justify-content-center align-items-center ">
+                        
+                        <div class="div">
+                            <p class="mx-4">Lunghezza Password: </p>
+                        </div>
+                        <div class="input-container">
+                            <input type="number" class="form-control" name="password">
+                        </div>
+                        
+                    </div>
+                    
+                    <!-- Buttons -->
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    <button type="reset" class="btn btn-secondary">Annulla</button>
+                </form>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
